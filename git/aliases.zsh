4ab48108feda482bb345b8078aa8dd1e54eb50b3
Hannes Lemberg <hanneslemberg@users.noreply.github.com>
2015-03-19 14:54:19 +0100

    gb git branch extended

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -2,7 +2,7 @@ alias glog="git log --follow --graph --pretty=format:'%Cred%h%Creset %an: %s - %
 alias gd='git diff -w'
 alias gc='git commit'
 alias gco='git checkout'
-alias gb='git branch'
+alias gb='git branch -vv -a'
 alias gs='git status -sb' 
 alias git-ignore-local='git update-index --assume-unchanged'
 
