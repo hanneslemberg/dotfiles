a9774ef02c51b8db95b8731dfdfb8f5d45906474
Hannes Lemberg <hanneslemberg@users.noreply.github.com>
2015-07-11 19:31:11 +0200

    tsc

--- js/aliases.zsh
+++ js/aliases.zsh
@@ -1,5 +1,6 @@
 alias npm-exec='PATH=$(npm bin):$PATH'
 alias grunt='$(npm bin)/grunt'
 alias tsd='$(npm bin)/tsd'
+alias tsc='$(npm bin)/tsc'
 alias gulp='$(npm bin)/gulp'
 alias bower='$(npm bin)/bower'
